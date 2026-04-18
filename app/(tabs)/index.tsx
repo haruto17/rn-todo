defc39ead78dc7619c0be80c355f1ec2cc7e6161
haruto17 <proaca157@gmail.com>
2026-04-18 17:01:38 +0900

    ✨️ アイコンボタンコンポーネントを実装

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,20 @@
-import { View } from 'react-native';
+import IconButton from '@/components/icon-button';
+import { StyleSheet, View } from 'react-native';
 
 export default function ListScreen() {
   return (
-    <View></View>
+    <View style={styles.container}>
+      <IconButton onPress={() => {}}/>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginRight: 24,
+    marginBottom: 24,
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end'
+  }
+})
