e90e104248a7077a0c47cdca5056f7d18b955138
haruto17 <proaca157@gmail.com>
2026-04-18 21:47:18 +0900

    🎨 タスク追加用のモーダルを配置、ボタンタップでモーダルが開くように変更

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,32 @@
 import IconButton from '@/components/icon-button';
+import TaskInputModal from '@/components/task-input-modal';
+import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 export default function ListScreen() {
+  const [isTaskInputModalVisible, setTaskInputModalVisible] = useState<boolean>(false)
+
+  const onTaskInputModalClose = () => {
+    setTaskInputModalVisible(false);
+  }
+
   return (
     <View style={styles.container}>
-      <IconButton iconName="add" onPress={() => {}}/>
+      <View style={styles.buttonContainer}>
+        <IconButton iconName="add" onPress={() => {
+          setTaskInputModalVisible(true);
+        }}/>
+      </View>
+      <TaskInputModal isVisible={isTaskInputModalVisible} onClose={onTaskInputModalClose}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  buttonContainer: {
     flex: 1,
     marginRight: 24,
     marginBottom: 24,
